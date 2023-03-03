ec5c3d3ee872a205260d6fa83e6068484d248fae
Tanner D Black <tannerdblack@gmail.com>
2023-03-03 12:55:07 -0500

    init

---
+++ MauiApp3/Platforms/Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace MauiApp3;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        var uiOptions =
+            SystemUiFlags.HideNavigation |
+            SystemUiFlags.LayoutHideNavigation |
+            SystemUiFlags.LayoutFullscreen |
+            SystemUiFlags.Fullscreen |
+            SystemUiFlags.LayoutStable |
+            SystemUiFlags.ImmersiveSticky;
+
+        if (Window == null) return;
+        Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+        this.Window?.AddFlags(WindowManagerFlags.Fullscreen);
+    }
+}
